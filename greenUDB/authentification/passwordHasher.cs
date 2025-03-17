713a91ebefddf4c8ad439835ed9858eee7eea694
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-17 14:08:12 +0100

    Add hash pass algo

---
+++ greenUDB/authentification/passwordHasher.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+    
+namespace GreenUApi.authentification
+{
+    public class passwordHasher
+    {
+        public string hasher(string password)
+        {
+        // Generate a 128-bit salt using a sequence of
+        // cryptographically strong random bytes.
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+            Convert.ToBase64String(salt);
+
+        // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+            return hashed;
+
+        /*
+         * SAMPLE OUTPUT
+         *
+         * Enter a password: Xtw9NMgx
+         * Salt: CGYzqeN4plZekNC88Umm1Q==
+         * Hashed: Gt9Yc4AiIvmsC1QQbe2RZsCIqvoYlst2xbz0Fs8aHnw=
+         */
+
+        }
+ 
+
+    }
+}
