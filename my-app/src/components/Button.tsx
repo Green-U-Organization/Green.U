03c250090321cd9701b40988c33fbe74005f089c
pedro88 <sandrep_85@hotmail.com>
2025-03-17 09:15:13 +0100

    some component

--- my-app/src/components/Button.tsx
+++ my-app/src/components/Button.tsx
@@ -25,23 +25,22 @@ const Button = ({ children }: Props) => {
                 onMouseUp={handleUp}
                 onMouseLeave={handleUp}>
 
-
                 <div className={`absolute -top-0 left-0 h-2 w-full bg-white`} style={{ display: buttonPush ? "block" : "none" }}></div>
 
-                {/* //bordure de base */}
+                {/* bordure de base */}
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} left-0 h-full w-2 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} right-0 h-full w-2 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} left-0 h-2 w-full bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "-bottom-2" : "bottom-0"} left-0 h-2 w-full bg-amber-500`}></div>
 
-                {/* //pixel intérieur */}
+                {/* pixel intérieur */}
                 <div className={`absolute ${buttonPush ? "top-4" : "top-2"} left-2 h-1 w-1 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "top-4" : "top-2"} right-2 h-1 w-1 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "bottom-0" : "bottom-2"} left-2 h-1 w-1 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "bottom-0" : "bottom-2"} right-2 h-1 w-1 bg-amber-500`}></div>
                 <div className={`absolute ${buttonPush ? "hidden" : "-bottom-2 border-b-1"} left-0 h-2 w-full bg-amber-600 `}></div>
 
-                {/* //pixel manquant extérieur */}
+                {/* pixel manquant extérieur */}
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} left-0 bg-amber-50 h-1 w-1`}></div>
                 <div className={`absolute ${buttonPush ? "top-3" : "top-1"} left-0 bg-amber-50 h-1 w-1`}></div>
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} left-1 bg-amber-50 h-1 w-1`}></div>
@@ -50,7 +49,7 @@ const Button = ({ children }: Props) => {
                 <div className={`absolute ${buttonPush ? "top-3" : "top-1"} right-0 bg-amber-50 h-1 w-1`}></div>
                 <div className={`absolute ${buttonPush ? "top-2" : "top-0"} right-1 bg-amber-50 h-1 w-1`}></div>
 
-                {/* //pixel manquant extérieur bordeur basse  */}
+                {/* pixel manquant extérieur bordeur basse  */}
                 <div className={`absolute ${buttonPush ? "-bottom-2 border-0" : "bottom-0 border-l-1 border-b-1"} left-0 bg-amber-600 h-1 w-1 `}></div>
                 <div className={`absolute ${buttonPush ? "-bottom-1 border-0" : "bottom-1  border-l-1"} left-0 bg-amber-600 h-1 w-1`}></div>
                 <div className={`absolute ${buttonPush ? "-bottom-2 border-0" : "bottom-0"} left-1 bg-amber-600 h-1 w-1`}></div>
@@ -59,7 +58,7 @@ const Button = ({ children }: Props) => {
                 <div className={`absolute ${buttonPush ? "-bottom-1 border-0" : "bottom-1 border-r-1"} right-0 bg-amber-600 h-1 w-1 `}></div>
                 <div className={`absolute ${buttonPush ? "-bottom-2 border-0" : "bottom-0"} right-1 bg-amber-600 h-1 w-1`}></div>
 
-                {/* //pixel manquant extérieur bordure basse  */}
+                {/* pixel manquant extérieur bordure basse  */}
                 <div className={`absolute ${buttonPush ? "border-0" : "border-r-1"} -bottom-1 left-0 bg-amber-50 h-1 w-1 `} ></div>
                 <div className={`absolute -bottom-2 left-0 bg-amber-50 h-1 w-1 `}></div>
                 <div className={`absolute ${buttonPush ? "border-0" : "border-t-1 border-r-1"} -bottom-2 left-1 bg-amber-50 h-1 w-1 `} ></div>
--- my-app/src/components/Checkbox.tsx
+++ my-app/src/components/Checkbox.tsx
@@ -1,8 +1,7 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Checkbox = () => {
-    const [buttonPush, setButtonPush] = useState(false)
+const Checkbox = ({check}) => {
     const [inside, setInside] = useState(false)
     const [clicked, setClicked] = useState(false)
 
@@ -14,7 +13,12 @@ const Checkbox = () => {
     }
     const handleClick = () => {
         setClicked(prev => !prev)
-    }
+    }    
+
+    useEffect(() => {
+        check = clicked
+        console.log(check)  
+    }, [clicked])
 
     return (
         <>
@@ -22,7 +26,11 @@ const Checkbox = () => {
             <div className={`h-5 w-5 relative m-5 ${inside ? "bg-amber-200" : "bg-white"}`}
             onMouseEnter={handleEnter}
             onMouseLeave={handleLeave}
-            onClick={handleClick}>
+            onClick={handleClick}
+            role="checkbox"
+            aria-checked={clicked}
+            tabIndex={0}
+            >
                 <div className={`absolute top-0 left-0 h-full w-1 bg-amber-500`}></div>
                 <div className={`absolute top-0 right-0 h-full w-1 bg-amber-500`}></div>
                 <div className={`absolute top-0 left-0 h-1 w-full bg-amber-500`}></div>
@@ -38,12 +46,20 @@ const Checkbox = () => {
                 <div className={`absolute bottom-0 left-0 h-0.5 w-0.5 bg-amber-50`}></div>
                 <div className={`absolute bottom-0 right-0 h-0.5 w-0.5 bg-amber-50`}></div>
 
-                <div className='h-full w-full absolute top-0 left-0' style={{display: clicked ? "block" : "none"}}>
-                    <div className='bg-amber-800 w-full h-full'></div>
-
+                <div className='h-full w-full relative top-0 left-0' style={{display: clicked ? "block" : "none"}}>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-1 left-1'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-1.5 left-1.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-2 left-2'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-2.5 left-2.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-3 left-3'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-3.5 left-3.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-1 left-3.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-1.5 left-3'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-2 left-2.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-2.5 left-2'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-3 left-1.5'></div>
+                    <div className='bg-amber-800 w-0.5 h-0.5 absolute top-3.5 left-1'></div>
                 </div>
-
-
             </div>
 
             <input type="checkbox" className=' w-5 h-5 bg-amber-400 border-0' />
