c77c9effac0fc3acdd18d3a1f6dbec63b50a8e79
EmmanuelCuiret <e.cuiret@gmail.com>
2025-03-25 08:59:39 +0100

    working on landing

--- my-app/src/app/landing/page.tsx
+++ my-app/src/app/landing/page.tsx
@@ -15,17 +15,17 @@ const page = () => {
 
 
     return (
-        <section className='grid grid-cols-4 grid-rows-7 h-screen w-screen gap-2 p-4'>
+        <section className='grid grid-cols-4 grid-rows-7 h-screen w-screen gap-2 p-2'>
 
             {/* Profile */}
             {/* <Card className='row-start-1 row-end-2 col-start-1 col-end-2 h-full w-full px-0 flex justify-center items-center'>
-        <Image src="/image/avatar/test-profile.png" alt="Profile" width={100} height={100} />
-        </Card> */}
+            <Image src="/image/avatar/test-profile.png" alt="Profile" width={100} height={100} />
+            </Card> */}
 
-            <div className="row-start-1 row-end-1 col-start-1 col-end-1 h-p-3">
+            <div className="row-start-1 row-end-2 col-start-1 col-end-2 h-full w-full flex justify-center">
                 <Link href="/profile">
                     <Image
-                        src={"/image/avatars/PI_01.png"}
+                        src={"/image/divers/profile.png"}
                         alt="Profile page"
                         width={96}
                         height={96}
@@ -38,16 +38,16 @@ const page = () => {
             {/* Post */}
             {/* <Card className='row-start-1 row-end-2 col-start-2 col-end-3 h-full w-full px-3'>
             Post
-        </Card> */}
+            </Card> */}
 
-            <div className='row-start-1 row-end-2 col-start-2 col-end-3 px-3'>
+            <div className="row-start-1 row-end-2 col-start-2 col-end-3 h-full w-full flex justify-center">
                 <Link href="/post">
                     <Image
-                        src=""
+                        src="/image/divers/post.png"
                         alt="Post page"
                         width={96}
                         height={96}
-                        title='Post'
+                        title="Post"
                         className="relative rounded-full border-2 border-border hover:border-4 transition-all duration-50"
                     />
                 </Link>
@@ -56,16 +56,16 @@ const page = () => {
             {/* Explore */}
             {/* <Card className='row-start-1 row-end-1 col-start-3 col-end-4 h-full w-full px-3'>
             Explore
-        </Card> */}
+            </Card> */}
 
-            <div className='row-start-1 row-end-1 col-start-3 col-end-4 h-full w-full px-3'>
+            <div className="row-start-1 row-end-2 col-start-3 col-end-4 h-full w-full flex justify-center">
                 <Link href="/explore">
                     <Image
-                        src=""
+                        src="/image/divers/explore.png"
                         alt="Explore page"
                         width={96}
                         height={96}
-                        title='Explore'
+                        title="Explore"
                         className="relative rounded-full border-2 border-border hover:border-4 transition-all duration-50"
                     />
                 </Link>
@@ -74,16 +74,16 @@ const page = () => {
             {/* Logout/Login */}
             {/* <Card className='row-start-1 row-end-2 col-start-4 col-end-5 h-full w-full px-3'>
             Login
-        </Card> */}
+            </Card> */}
 
-            <div className='row-start-1 row-end-2 col-start-4 col-end-5  h-full w-full px-3'>
+            <div className="row-start-1 row-end-2 col-start-4 col-end-5 h-full w-full flex justify-center">
                 <Link href="/login">
                     <Image
-                        src=""
+                        src="/image/divers/logged-out.png"
                         alt="Login page"
                         width={96}
                         height={96}
-                        title='Login'
+                        title="Login"
                         className="relative rounded-full border-2 border-border hover:border-4 transition-all duration-50"
                     />
                 </Link>
