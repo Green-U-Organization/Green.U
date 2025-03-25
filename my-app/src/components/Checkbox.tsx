16b0cb865974cdde39d7d642acc8e3f43ddd8c71
LucasC <lucascloes@gmail.com>
2025-03-25 13:49:03 +0100

    Revert "backend-dev"
    This reverts commit 78765579bf6246950aae442a3a7f5ceb13650950.

---
+++ my-app/src/components/Checkbox.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+const Checkbox = ({check}) => {
+    const [inside, setInside] = useState(false)
+    const [clicked, setClicked] = useState(false)
+
+    const handleEnter = () => {
+        setInside(true)
+    }
+    const handleLeave = () => {
+        setInside(false)
+    }
+    const handleClick = () => {
+        setClicked(prev => !prev)
+    }    
+
+    useEffect(() => {
+        check = clicked
+    }, [clicked])
+
+    return (
+        <>
+
+            <div className={`h-5 w-5 relative m-5 ${inside ? "bg-bgbutton" : "bg-cardbackground"}`}
+            onMouseEnter={handleEnter}
+            onMouseLeave={handleLeave}
+            onClick={handleClick}
+            role="checkbox"
+            aria-checked={clicked}
+            tabIndex={0}
+            >
+                <div className={`absolute top-0 left-0 h-full w-1 bg-border`}></div>
+                <div className={`absolute top-0 right-0 h-full w-1 bg-border`}></div>
+                <div className={`absolute top-0 left-0 h-1 w-full bg-border`}></div>
+                <div className={`absolute bottom-0 left-0 h-1 w-full bg-border`}></div>
+
+                <div className={`absolute top-1 left-1 h-0.5 w-0.5 bg-border`}></div>
+                <div className={`absolute top-1 right-1 h-0.5 w-0.5 bg-border`}></div>
+                <div className={`absolute bottom-1 left-1 h-0.5 w-0.5 bg-border`}></div>
+                <div className={`absolute bottom-1 right-1 h-0.5 w-0.5 bg-border`}></div>
+   
+                <div className={`absolute top-0 left-0 h-0.5 w-0.5 bg-extbutton`}></div>
+                <div className={`absolute top-0 right-0 h-0.5 w-0.5 bg-extbutton`}></div>
+                <div className={`absolute bottom-0 left-0 h-0.5 w-0.5 bg-extbutton`}></div>
+                <div className={`absolute bottom-0 right-0 h-0.5 w-0.5 bg-extbutton`}></div>
+
+                <div className='h-full w-full relative top-0 left-0' style={{display: clicked ? "block" : "none"}}>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-1 left-1'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-1.5 left-1.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-2 left-2'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-2.5 left-2.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-3 left-3'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-3.5 left-3.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-1 left-3.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-1.5 left-3'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-2 left-2.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-2.5 left-2'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-3 left-1.5'></div>
+                    <div className='bg-shadow w-0.5 h-0.5 absolute top-3.5 left-1'></div>
+                </div>
+            </div>
+
+        </>
+
+
+    )
+}
+
+export default Checkbox
