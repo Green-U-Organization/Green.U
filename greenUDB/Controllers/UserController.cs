0f2fbcc675d4176ed160a836836d0a4d112bf218
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-02 14:38:36 +0200

    Complete the usercontroller

--- greenUDB/Controllers/UserController.cs
+++ greenUDB/Controllers/UserController.cs
@@ -2,9 +2,38 @@ using Microsoft.EntityFrameworkCore;
 using GreenUApi.authentification;
 using Microsoft.AspNetCore.Mvc;
 using GreenUApi.Models;
-using Microsoft.AspNetCore.Mvc;
 
 namespace GreenUApi.Controllers;
+
+public class UserModification
+{
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Salt { get; set; }
+
+    public bool? IsAdmin { get; set; }
+
+    public string? Firstname { get; set; } 
+
+    public string? Lastname { get; set; } 
+
+    public string? Email { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? Country { get; set; } 
+
+    public string? Gender { get; set; }
+
+    public DateOnly Birthday { get; set; }
+
+    public string? ProfileImage { get; set; }
+
+    public string?   Bio { get; set; }
+}
 
 [Route("api/[controller]")]
 [ApiController]
@@ -50,11 +79,11 @@ public class UserController : ControllerBase
         _db.Users.Add(user);
         await _db.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        return Ok("User are created !");
     }
 
     [HttpPatch("{id}")]
-    public async Task<IActionResult> UpdateUser(int id, User inputUser)
+    public async Task<IActionResult> UpdateUser(long id, UserModification modification)
     {
         var user = await _db.Users.FindAsync(id);
 
@@ -63,25 +92,90 @@ public class UserController : ControllerBase
             return NotFound();
         }
 
-        user.Firstname = inputUser.Firstname;
+        if (!string.IsNullOrEmpty(modification.Username))
+        {
+            user.Username = modification.Username;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Password))
+        {
+            user.Password = modification.Password;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Salt))
+        {
+            user.Salt = modification.Salt;
+        }
+
+        if (modification.IsAdmin.HasValue)
+        {
+            user.IsAdmin = modification.IsAdmin.Value;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Firstname))
+        {
+            user.Firstname = modification.Firstname;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Lastname))
+        {
+            user.Lastname = modification.Lastname;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Email))
+        {
+            user.Email = modification.Email;
+        }
+
+        if (!string.IsNullOrEmpty(modification.PostalCode))
+        {
+            user.PostalCode = modification.PostalCode;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Country))
+        {
+            user.Country = modification.Country;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Gender))
+        {
+            user.Gender = modification.Gender;
+        }
+
+        if (modification.Birthday != null)
+        {
+            user.Birthday = modification.Birthday;
+        }
+
+        if (!string.IsNullOrEmpty(modification.ProfileImage))
+        {
+            user.ProfileImage = modification.ProfileImage;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Bio))
+        {
+            user.Bio = modification.Bio;
+        }
+
+        _db.Users.Update(user);
         await _db.SaveChangesAsync();
 
-        return NoContent();
+        return Ok("User is modified");
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteUser(int id)
+    public async Task<IActionResult> DeleteUser(long id)
     {
         var user = await _db.Users.FindAsync(id);
 
         if (user == null)
         {
-            return NotFound();
+            return NotFound("Incorrect ID");
         }
 
         _db.Users.Remove(user);
         await _db.SaveChangesAsync();
 
-        return NoContent();
+        return Ok("DELETE SUCCES");
     }
 }
