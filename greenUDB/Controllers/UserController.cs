73f81da660caba2e857579da055ef32285dbbea0
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-02 16:51:28 +0200

    Add new Api

---
+++ greenUDB/Controllers/UserController.cs
@@ -0,0 +1,181 @@
+using Microsoft.EntityFrameworkCore;
+using GreenUApi.authentification;
+using Microsoft.AspNetCore.Mvc;
+using GreenUApi.Models;
+
+namespace GreenUApi.Controllers;
+
+public class UserModification
+{
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Salt { get; set; }
+
+    public bool? IsAdmin { get; set; }
+
+    public string? Firstname { get; set; } 
+
+    public string? Lastname { get; set; } 
+
+    public string? Email { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? Country { get; set; } 
+
+    public string? Gender { get; set; }
+
+    public DateOnly Birthday { get; set; }
+
+    public string? ProfileImage { get; set; }
+
+    public string?   Bio { get; set; }
+}
+
+[Route("api/user")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly GreenUDB _db;
+
+    public UserController(GreenUDB db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(long id)
+    {
+        var user = await _db.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(User user)
+    {
+        var userDbData = await _db.Users
+           .Where(u => u.Username == user.Username)
+           .Select(u => new User { Username = u.Username })
+           .ToArrayAsync();
+
+        if (userDbData.Length != 0)
+        {
+            return Conflict(new { message = "This username already exists" });
+        }
+
+        string[] hashSalt = Authentification.Hasher(user.Password, null);
+        user.Password = hashSalt[0];
+        user.Salt = hashSalt[1];
+
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync();
+
+        return Ok("User are created !");
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateUser(long id, UserModification modification)
+    {
+        var user = await _db.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        if (!string.IsNullOrEmpty(modification.Username))
+        {
+            user.Username = modification.Username;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Password))
+        {
+            user.Password = modification.Password;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Salt))
+        {
+            user.Salt = modification.Salt;
+        }
+
+        if (modification.IsAdmin.HasValue)
+        {
+            user.IsAdmin = modification.IsAdmin.Value;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Firstname))
+        {
+            user.Firstname = modification.Firstname;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Lastname))
+        {
+            user.Lastname = modification.Lastname;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Email))
+        {
+            user.Email = modification.Email;
+        }
+
+        if (!string.IsNullOrEmpty(modification.PostalCode))
+        {
+            user.PostalCode = modification.PostalCode;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Country))
+        {
+            user.Country = modification.Country;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Gender))
+        {
+            user.Gender = modification.Gender;
+        }
+
+        if (modification.Birthday != null)
+        {
+            user.Birthday = modification.Birthday;
+        }
+
+        if (!string.IsNullOrEmpty(modification.ProfileImage))
+        {
+            user.ProfileImage = modification.ProfileImage;
+        }
+
+        if (!string.IsNullOrEmpty(modification.Bio))
+        {
+            user.Bio = modification.Bio;
+        }
+
+        _db.Users.Update(user);
+        await _db.SaveChangesAsync();
+
+        return Ok("User is modified");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(long id)
+    {
+        var user = await _db.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound("Incorrect ID");
+        }
+
+        _db.Users.Remove(user);
+        await _db.SaveChangesAsync();
+
+        return Ok("DELETE SUCCES");
+    }
+}
---
+++ greenUDB/authentification/authentification.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using GreenUApi.Controllers;
+using Token;
+using System.Text;
+using GreenUApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenUApi.authentification
+{
+    public class Authentification
+    {
+        public static string[] Hasher(string password, byte[]? salty)
+        {
+            // Generate a 128-bit salt using a sequence of
+            // cryptographically strong random bytes.
+            byte[] salt;
+            if(salty != null){
+                salt = salty;
+            }
+            else{
+                salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+            }
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+            return [hashed, Convert.ToBase64String(salt)];
+
+        }
+
+    public static async Task<IResult> Login(string username, string password, GreenUDB db)
+        {
+            var User = await db.Users
+            .Where(u => u.Username == username)
+            .Select(u => new User
+            {
+                Id = u.Id,
+                Username = u.Username,
+                Password = u.Password,
+                Salt = u.Salt
+            })
+            .FirstOrDefaultAsync();
+
+            if (User == null)
+            {
+                return TypedResults.Unauthorized();
+            }
+
+
+            String hashedPassword = "";
+            if(User.Salt != null)
+                hashedPassword = Hasher(password, Convert.FromBase64String(User.Salt))[0];
+
+            if (User.Password == hashedPassword)
+            {
+                var token = Jwt.GenerateJwtToken(User);
+
+                return TypedResults.Ok(new { message = "Mot de passe valide !", token });
+            }
+
+            return TypedResults.Unauthorized();
+        }
+
+
+    }
+}
