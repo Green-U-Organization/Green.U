0fa310c8fa9e982eb281bee9132945a7cf16e98f
EmmanuelCuiret <e.cuiret@gmail.com>
2025-04-07 11:09:12 +0200

    fix

--- my-app/src/components/Landing.tsx
+++ my-app/src/components/Landing.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Card from '../components/UI/Card';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
--- my-app/src/components/UI/Button.tsx
+++ my-app/src/components/UI/Button.tsx
@@ -1,59 +1,45 @@
 'use client';
-import React, {
-  FC,
-  FormEvent,
-  MouseEvent,
-  PropsWithChildren,
-  useState,
-} from 'react';
-import { useRouter } from 'next/navigation';
+
+import Link from 'next/link';
+import React, { FormEvent, MouseEvent, useState } from 'react';
 
 type ButtonProps = {
-<<<<<<< HEAD
   children: React.ReactNode;
   href?: string;
   type: 'link' | 'submit' | 'button' | 'action';
   handleSubmit?: (e: FormEvent<HTMLButtonElement>) => void;
   handleAction?: (e: FormEvent<HTMLButtonElement>) => void;
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-  disabled?: boolean;
-  title?: string;
 };
 
-const Button: FC<PropsWithChildren<ButtonProps>> = ({
+const Button = ({
   children,
   href,
   type,
   handleSubmit,
   handleAction,
   onClick,
-  disabled,
-  title,
-}) => {
-=======
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
-};
-
-const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, onClick }) => {
->>>>>>> f4127f1daf37b7f02d7b6b13c6a06d379d1a5e1e
+}: ButtonProps) => {
   const [buttonPush, setButtonPush] = useState(false);
   const [inside, setInside] = useState(false);
 
   const handleDown = () => {
     setButtonPush(true);
-<<<<<<< HEAD
-    // if (type === 'submit' && handleSubmit)
-    //   new Event('submit') as unknown as FormEvent<HTMLButtonElement>;
-    // if (type === 'action' && handleAction)
-    //   new Event('action') as unknown as FormEvent<HTMLButtonElement>;
-=======
-    new Event('action') as unknown as FormEvent<HTMLButtonElement>;
->>>>>>> f4127f1daf37b7f02d7b6b13c6a06d379d1a5e1e
+    if (type === 'submit' && handleSubmit)
+      new Event('submit') as unknown as FormEvent<HTMLButtonElement>;
+    if (type === 'action' && handleAction)
+      new Event('action') as unknown as FormEvent<HTMLButtonElement>;
   };
 
   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    onClick(e);
+    if (type === 'submit' && handleSubmit)
+      handleSubmit(e as unknown as FormEvent<HTMLButtonElement>);
+    if (type === 'action' && handleAction) {
+      e.preventDefault();
+      handleAction(e as unknown as FormEvent<HTMLButtonElement>);
+    }
+
+    if (onClick) onClick(e);
   };
 
   const handleUp = () => setButtonPush(false);
@@ -63,11 +49,12 @@ const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, onClick }) => {
     setButtonPush(false);
   };
 
-  return (
-<<<<<<< HEAD
-    <ButtonWrapper>
+  return type === 'link' && href ? (
+    <Link href={href}>
       <button
-        className={`bg-button relative m-5 px-6 py-2 text-2xl select-none ${inside ? 'bg-bgbutton' : 'bg-cardbackground'} cursor-pointer disabled:cursor-not-allowed disabled:opacity-50`}
+        className={`bg-button relative m-5 px-6 py-2 text-2xl select-none ${
+          inside ? 'bg-bgbutton' : 'bg-cardbackground'
+        }`}
         onMouseDown={handleDown}
         onMouseUp={handleUp}
         onMouseLeave={handleLeave}
@@ -75,16 +62,164 @@ const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, onClick }) => {
         onClick={handleClick}
         onTouchStart={handleDown}
         onTouchEnd={handleUp}
-        disabled={disabled}
-        title={title}
       >
         <div
-          className={`bg-extbutton absolute top-0 left-0 h-2 w-full`}
+          className={`bg-extbutton absolute -top-0 left-0 h-2 w-full`}
           style={{ display: buttonPush ? 'block' : 'none' }}
-        />
-=======
+        ></div>
+
+        {/* bordure de base */}
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-border left-0 h-full w-2`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-border right-0 h-full w-2`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-border left-0 h-2 w-full`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-2' : 'bottom-0'
+          } bg-border left-0 h-2 w-full`}
+        ></div>
+
+        {/* pixel intérieur */}
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-4' : 'top-2'
+          } bg-border left-2 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-4' : 'top-2'
+          } bg-border right-2 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'bottom-0' : 'bottom-2'
+          } bg-border left-2 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'bottom-0' : 'bottom-2'
+          } bg-border right-2 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'hidden' : '-bottom-2 border-b-1'
+          } bg-shadow left-0 h-2 w-full`}
+        ></div>
+
+        {/* pixel manquant extérieur */}
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-extbutton left-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-3' : 'top-1'
+          } bg-extbutton left-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-extbutton left-1 h-1 w-1`}
+        ></div>
+
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-extbutton right-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-3' : 'top-1'
+          } bg-extbutton right-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'top-2' : 'top-0'
+          } bg-extbutton right-1 h-1 w-1`}
+        ></div>
+
+        {/* pixel manquant extérieur bordeur basse  */}
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-2 border-0' : 'bottom-0 border-b-1 border-l-1'
+          } bg-shadow left-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-1 border-0' : 'bottom-1 border-l-1'
+          } bg-shadow left-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-2 border-0' : 'bottom-0'
+          } bg-shadow left-1 h-1 w-1`}
+        ></div>
+
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-2 border-0' : 'bottom-0 border-r-1 border-b-1'
+          } bg-shadow right-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-1 border-0' : 'bottom-1 border-r-1'
+          } bg-shadow right-0 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? '-bottom-2 border-0' : 'bottom-0'
+          } bg-shadow right-1 h-1 w-1`}
+        ></div>
+
+        {/* pixel manquant extérieur bordure basse  */}
+        <div
+          className={`absolute ${
+            buttonPush ? 'border-0' : 'border-r-1'
+          } bg-extbutton -bottom-1 left-0 h-1 w-1`}
+        ></div>
+        <div className={`bg-extbutton absolute -bottom-2 left-0 h-1 w-1`}></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'border-0' : 'border-t-1 border-r-1'
+          } bg-extbutton -bottom-2 left-1 h-1 w-1`}
+        ></div>
+
+        <div
+          className={`absolute ${
+            buttonPush ? 'border-0' : 'border-l-1'
+          } bg-extbutton right-0 -bottom-1 h-1 w-1`}
+        ></div>
+        <div
+          className={`bg-extbutton absolute right-0 -bottom-2 h-1 w-1`}
+        ></div>
+        <div
+          className={`absolute ${
+            buttonPush ? 'border-0' : 'border-t-1 border-l-1'
+          } bg-extbutton right-1 -bottom-2 h-1 w-1`}
+        ></div>
+
+        <p className={`relative ${buttonPush ? 'top-2' : 'top-0'}`}>
+          {children}
+        </p>
+      </button>
+    </Link>
+  ) : (
     <button
-      className={`bg-button relative m-5 px-6 py-2 text-2xl select-none ${inside ? 'bg-bgbutton' : 'bg-cardbackground'} cursor-pointer disabled:cursor-not-allowed disabled:opacity-50`}
+      className={`bg-button relative m-5 px-6 py-2 text-2xl select-none ${
+        inside ? 'bg-bgbutton' : 'bg-cardbackground'
+      } cursor-pointer`}
       onMouseDown={handleDown}
       onMouseUp={handleUp}
       onMouseLeave={handleLeave}
@@ -92,154 +227,151 @@ const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, onClick }) => {
       onClick={handleClick}
       onTouchStart={handleDown}
       onTouchEnd={handleUp}
-      // disabled={disabled}
-      // title={title}
     >
       <div
         className={`bg-extbutton absolute top-0 left-0 h-2 w-full`}
         style={{ display: buttonPush ? 'block' : 'none' }}
-      />
->>>>>>> f4127f1daf37b7f02d7b6b13c6a06d379d1a5e1e
+      ></div>
 
       {/* bordure de base */}
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-border left-0 h-full w-2`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-border right-0 h-full w-2`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-border left-0 h-2 w-full`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? '-bottom-2' : 'bottom-0'
         } bg-border left-0 h-2 w-full`}
-      />
+      ></div>
 
       {/* pixel intérieur */}
       <div
         className={`absolute ${
           buttonPush ? 'top-4' : 'top-2'
         } bg-border left-2 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-4' : 'top-2'
         } bg-border right-2 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'bottom-0' : 'bottom-2'
         } bg-border left-2 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'bottom-0' : 'bottom-2'
         } bg-border right-2 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'hidden' : '-bottom-2 border-b-1'
         } bg-shadow left-0 h-2 w-full`}
-      />
+      ></div>
 
       {/* pixel manquant extérieur */}
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-extbutton left-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-3' : 'top-1'
         } bg-extbutton left-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-extbutton left-1 h-1 w-1`}
-      />
+      ></div>
 
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-extbutton right-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-3' : 'top-1'
         } bg-extbutton right-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? 'top-2' : 'top-0'
         } bg-extbutton right-1 h-1 w-1`}
-      />
+      ></div>
 
       {/* pixel manquant extérieur bordeur basse  */}
       <div
         className={`absolute ${
           buttonPush ? '-bottom-2 border-0' : 'bottom-0 border-b-1 border-l-1'
         } bg-shadow left-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? '-bottom-1 border-0' : 'bottom-1 border-l-1'
         } bg-shadow left-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? '-bottom-2 border-0' : 'bottom-0'
         } bg-shadow left-1 h-1 w-1`}
-      />
+      ></div>
 
       <div
         className={`absolute ${
           buttonPush ? '-bottom-2 border-0' : 'bottom-0 border-r-1 border-b-1'
         } bg-shadow right-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? '-bottom-1 border-0' : 'bottom-1 border-r-1'
         } bg-shadow right-0 h-1 w-1`}
-      />
+      ></div>
       <div
         className={`absolute ${
           buttonPush ? '-bottom-2 border-0' : 'bottom-0'
         } bg-shadow right-1 h-1 w-1`}
-      />
+      ></div>
 
       {/* pixel manquant extérieur bordure basse  */}
       <div
         className={`absolute ${
           buttonPush ? 'border-0' : 'border-r-1'
         } bg-extbutton -bottom-1 left-0 h-1 w-1`}
-      />
-      <div className={`bg-extbutton absolute -bottom-2 left-0 h-1 w-1`} />
+      ></div>
+      <div className={`bg-extbutton absolute -bottom-2 left-0 h-1 w-1`}></div>
       <div
         className={`absolute ${
           buttonPush ? 'border-0' : 'border-t-1 border-r-1'
         } bg-extbutton -bottom-2 left-1 h-1 w-1`}
-      />
+      ></div>
 
       <div
         className={`absolute ${
           buttonPush ? 'border-0' : 'border-l-1'
         } bg-extbutton right-0 -bottom-1 h-1 w-1`}
-      />
-      <div className={`bg-extbutton absolute right-0 -bottom-2 h-1 w-1`} />
+      ></div>
+      <div className={`bg-extbutton absolute right-0 -bottom-2 h-1 w-1`}></div>
       <div
         className={`absolute ${
           buttonPush ? 'border-0' : 'border-t-1 border-l-1'
         } bg-extbutton right-1 -bottom-2 h-1 w-1`}
-      />
+      ></div>
 
       <p className={`relative ${buttonPush ? 'top-2' : 'top-0'}`}>{children}</p>
     </button>
