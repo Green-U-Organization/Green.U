a68ffc30a305f2877572627e849d95d03f82d39e
pedro88 <sandrep_85@hotmail.com>
2025-04-15 17:01:09 +0200

    working on parcel creation

--- my-app/src/components/Molecule/NewParcelForm.tsx
+++ my-app/src/components/Molecule/NewParcelForm.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import TextInput from '../Atom/TextInput';
+import React, { useState } from 'react';
 import Button from '../Atom/Button';
 import H2 from '../Atom/H2';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { createNewGarden } from '@/utils/actions/garden/createNewGarden';
 
 const NewParcelForm: React.FC<{ displayCondition: boolean }> = ({
   displayCondition,
 }) => {
+  const [length, setLength] = useState(1);
+  const [width, setWidth] = useState(1);
+  const actualGarden = useSelector(
+    (state: RootState) => state.garden.selectedGarden
+  );
   if (!displayCondition) return null;
 
   const handleSubmit = () => {
-    console.log('create');
+    const newParcel = {
+      gardenId: actualGarden?.id,
+      length: length,
+      width: width,
+      nLine: 1,
+      parcelAngle: 0,
+    };
+
+    console.log(newParcel);
+    createNewGarden(newParcel);
+  };
+
+  const handleLengthChange = (e: { target: { value: string } }) => {
+    const newValue = parseFloat(e.target.value);
+    setLength(newValue);
+    console.log(length);
+  };
+  const handleWidthChange = (e: { target: { value: string } }) => {
+    const newValue = parseFloat(e.target.value);
+    setWidth(newValue);
+    console.log(length);
   };
 
   return (
-    <div className="bg-gardenBG flex h-[80vw] w-[60vw] flex-col items-center justify-between rounded-xl border-2 p-4">
+    <div className="bg-gardenBG flex h-[auto] w-[60vw] flex-col items-center justify-between rounded-xl border-2 p-4">
       <H2>Add parcel</H2>
       <form className="flex flex-col items-center">
-        <TextInput label="Length"></TextInput>
-
-        <TextInput label="Width"></TextInput>
+        {/* <TextInput label="Length"></TextInput> */}
+        <label htmlFor="">Length : {length}m</label>
+        <div className="flex w-full items-center justify-around">
+          <p onClick={() => setLength((prev) => prev - 1)}>-</p>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            step="0.5"
+            value={length}
+            onChange={handleLengthChange}
+            className={`bg-border mx-3 h-2 cursor-cell appearance-none`}
+          />
+          <p onClick={() => setLength((prev) => prev + 1)}>+</p>
+        </div>
+        {/* <TextInput label="Width"></TextInput> */}
+        <label htmlFor="">Width : {width}m</label>
+        <div className="flex w-full items-center justify-around">
+          <p onClick={() => setWidth((prev) => prev - 0.5)}>-</p>
+          <input
+            type="range"
+            min="0"
+            max="20"
+            step="0.1"
+            value={width}
+            onChange={handleWidthChange}
+            className={`bg-border mx-3 h-2 cursor-cell appearance-none`}
+          />
+          <p onClick={() => setWidth((prev) => prev + 0.5)}>+</p>
+        </div>
 
         <Button onClick={handleSubmit}>Create!</Button>
       </form>
