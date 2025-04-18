e43bef04111343fd07bffeabdf838612b43f2b4a
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-18 15:20:21 +0200

    handle error for CreateGarden

--- greenUDB/Controllers/GardenController.cs
+++ greenUDB/Controllers/GardenController.cs
@@ -11,25 +11,23 @@ namespace GreenUApi.Controllers
     public class GardenController : ControllerBase
     {
 
-        public class GardenDto{
-            public long? Id { get; set; }
-            public long AuthorId { get; set; }
+        public class GardenModification{
 
-            public string Name { get; set; } = null!;
+            public string? Name { get; set; } = null!;
 
-            public string Description { get; set; } = null!;
+            public string? Description { get; set; } = null!;
 
-            public double Latitude { get; set; }
+            public double? Latitude { get; set; }
 
-            public double Longitude { get; set; }
+            public double? Longitude { get; set; }
 
-            public double Length { get; set; }
+            public double? Length { get; set; }
 
-            public double Width { get; set; }
+            public double? Width { get; set; }
 
-            public GardenPrivacy Privacy { get; set; } = GardenPrivacy.Public;
+            public GardenPrivacy? Privacy { get; set; } = GardenPrivacy.Public;
 
-            public GardenType Type { get; set; } = GardenType.Personnal;
+            public GardenType? Type { get; set; } = GardenType.Personnal;
 
         }
         private readonly GreenUDB _db   ;
@@ -61,93 +59,134 @@ namespace GreenUApi.Controllers
             return Ok(new { isEmpty = false, message = "All garden", content = gardens });
         }
 
-       [HttpGet("user/{userId}")]
-        public async Task<ActionResult<IEnumerable<GardenDto>>> GetGardensByUser(long userId)
+       [HttpGet("user/{id}")]
+        public async Task<ActionResult<IEnumerable<GardenDto>>> GetGardensByUser(long id)
         {
             var gardens = await _db.Gardens
-                                        .Where(g => g.AuthorId == userId)
-                                        .Select(g => new GardenDto
-                                        {
-                                            Id = g.Id,
-                                            AuthorId = g.AuthorId,
-                                            Name = g.Name,
-                                            Description = g.Description,
-                                            Latitude = g.Latitude,
-                                            Longitude = g.Longitude,
-                                            Length = g.Length,
-                                            Width = g.Width,
-                                            Privacy = g.Privacy,
-                                            Type = g.Type
-                                        })
+                                        .Where(g => g.AuthorId == id)
                                         .ToListAsync();
 
             if (gardens == null || gardens.Count == 0)
             {
-                return NotFound();
+                return NotFound(new { isEmpty = true, message = "This user didn't have garden or user doesn't exist" });
             }
 
-            return Ok(gardens);
+            return Ok(new { isEmpty = false, message = "All garden by user ID", content = gardens});
         }
 
         [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchGarden(long id, Garden garden)
+        public async Task<IActionResult> PatchGarden(long id, GardenModification modification)
         {
-            garden.Id = id;
-            _db.Entry(garden).State = EntityState.Modified;
+            var garden = await _db.Gardens
+                .FindAsync(id);
 
-            try
+            if (garden == null)
             {
-                await _db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GardenExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return NotFound(new { isEmpty = true, message = "The garden id is incorrect" });
             }
 
-            return NoContent();
+            if (garden.Deleted)
+            {
+                return BadRequest(new { isEmpty = true, message = "This garden is deleted" });
+            }
+
+            if (!string.IsNullOrEmpty(modification.Name))
+            {
+                garden.Name = modification.Name;
+            }
+
+            if (!string.IsNullOrEmpty(modification.Description))
+            {
+                garden.Description = modification.Description;
+            }
+
+            if (modification.Latitude.HasValue)
+            {
+                garden.Latitude = modification.Latitude.Value;
+            }
+
+            if (modification.Longitude.HasValue)
+            {
+                garden.Longitude = modification.Longitude.Value;
+            }
+
+            if (modification.Length.HasValue)
+            {
+                garden.Length = modification.Length.Value;
+            }
+
+            if (modification.Width.HasValue)
+            {
+                garden.Width = modification.Width.Value;
+            }
+
+            if (modification.Privacy.HasValue)
+            {
+                garden.Privacy = modification.Privacy.Value;
+            }
+
+            if (modification.Type.HasValue)
+            {
+                garden.Type = modification.Type.Value;
+            }
+
+            _db.Update(garden);
+            await _db.SaveChangesAsync();
+
+            return Ok(); 
         }
 
         [HttpPost]
-        public async Task<ActionResult<Garden>> PostGarden([FromBody] GardenDto garden)
+        public async Task<ActionResult<Garden>> PostGarden([FromBody] Garden garden)
         {
-
-            var newGarden = new Garden {
-                AuthorId = garden.AuthorId,
-                Name = garden.Name,
-                Description = garden.Description,
-                Latitude = garden.Latitude,
-                Longitude = garden.Longitude,
-                Length = garden.Length,
-                Width = garden.Width,
-                Privacy = garden.Privacy,
-                Type = garden.Type
-            };
-
-            if (newGarden == null)
+            if (garden == null)
             {
-                return BadRequest("Invalid garden data.");
+                return BadRequest(new { isEmpty = true, message = "No body..." });
             }
 
-            var userExists = await _db.Users.AnyAsync(u => u.Id == garden.AuthorId);
-            if (!userExists)
+            var ExsistingGarden = await _db.Gardens
+                .Where(g => g.Name == garden.Name)
+                .FirstOrDefaultAsync();
+
+            if (ExsistingGarden != null)
             {
-                return BadRequest("The specified authorId does not exist.");
+                return Conflict(new { isEmpty = true, message = "This garden name is taken..." });
             }
 
-            _db.Gardens.Add(newGarden);
-            await _db.SaveChangesAsync();
+            if (string.IsNullOrWhiteSpace(garden.Name))
+            {
+                return BadRequest(new { isEmpty = true, message = "Need garden Name" });
+            }
 
-            return CreatedAtAction(nameof(GetGarden), new { id = newGarden.Id }, newGarden);
+            if (string.IsNullOrWhiteSpace(garden.Description))
+            {
+                return BadRequest(new { isEmpty = true, message = "Description garden is required" });
+            }
+
+            if (garden.Latitude < -90 || garden.Latitude > 90)
+            {
+                return BadRequest(new { isEmpty = true, message = "The latitude need a double in this range -90 or 90" });
+            }
+
+            if (garden.Longitude < -180 || garden.Longitude > 180)
+            {
+                return BadRequest(new { isEmpty = true, message = "The longitude need a double in this range -180 or 180" });
+            }
+
+            if (garden.Length <= 0)
+            {
+                return BadRequest(new { isEmpty = true, message = "The garden length need to be above 0" });
+            }
+
+            if (garden.Width <= 0)
+            {
+                return BadRequest(new { isEmpty = true, message = "The garden width need to be above 0" });
+            }
+
+
         }
 
-        [HttpPatch("delete/{id}")]
+            [HttpPatch("delete/{id}")]
         public async Task<IActionResult> DeleteGarden(long id)
         {
             try
