5b7426c0c1fb318dd7f7d2ac3dcbf57ee5854852
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-24 15:24:10 +0200

    create the ContributorController.cs

---
+++ greenUDB/Controllers/ContributorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GreenUApi.Models;
+
+namespace GreenUApi.Controllers
+{
+    [Route("contributor")]
+    [ApiController]
+    public class ContributorController : ControllerBase
+    {
+        private readonly GreenUDB _db;
+
+        public ContributorController(GreenUDB context)
+        {
+            _db = context;
+        }
+
+    }
+}
