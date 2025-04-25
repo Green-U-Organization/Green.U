3121e97f80a942342c8bd58e61ddf675daf57fe4
pedro88 <sandrep_85@hotmail.com>
2025-04-25 09:28:06 +0200

    build

--- my-app/src/components/Atom/VegetableIcon.tsx
+++ my-app/src/components/Atom/VegetableIcon.tsx
@@ -12,7 +12,6 @@ const VegetableIcon = ({ id }: VegetableIconProps) => {
   return (
     <>
       {crops?.content[0]?.icon && crops.content[0].icon !== '' && (
-        // eslint-disable-next-line @next/next/no-img-element
         <img src={crops.content[0].icon} alt="" className="w-[6vw]" />
       )}
     </>
