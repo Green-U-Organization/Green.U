83600ecb12745bb51e7146a47c54693aa8421a85
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-06-02 15:53:28 +0200

    Add post route to create a Harvest

--- greenUDB/Controllers/HarvestController.cs
+++ greenUDB/Controllers/HarvestController.cs
@@ -12,9 +12,31 @@ namespace GreenUApi.Controllers
 
         private readonly GreenUDB _db;
 
+        public HarvestController(GreenUDB db)
+        {
+            _db = db;
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateHarvest([FromBody] Harvest harvest)
         {
+            var Crop = await _db.Crops
+                .Where(c => c.Id == harvest.CropId)
+                .Select(c => new
+                {
+                    c.Id,
+                    c.GardenId
+                })
+                .FirstOrDefaultAsync();
+
+            if (Crop == null) return NotFound(new {isEmpty = true, message = "CropId is wrong..."});
+
+            harvest.GardenId = Crop.GardenId;
+
+            _db.Add(harvest);
+            await _db.SaveChangesAsync();
+
+            return Ok(new { isEmpty = false, message = "Harvest is created !", content = harvest });
 
         }
     }
