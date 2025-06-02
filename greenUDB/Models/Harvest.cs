c3e0bffdeb408c1963fccc0e2967daaa5aebbd0f
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-06-02 12:17:39 +0200

    Create a new table Harvest

---
+++ greenUDB/Models/Harvest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreenUApi.Models
+{
+    public partial class Harvest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public long? Id { get; set; }
+
+        public long? GardenId { get; set; }
+
+        public long? CropId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
